cec29cacf190fcda51d0ac2193d2cc9170592bc9
Work <Work@Hengs-MacBook-Air.local>
2023-04-12 12:01:15 -0400

    small picture added; figma pictures correctly downloaded

--- virgocx/src/pages/App.tsx
+++ virgocx/src/pages/App.tsx
@@ -17,12 +17,20 @@ class Main extends React.Component {
     window.removeEventListener('resize', this.updateDimensions);
   }
   render() {
-    let title;
+    let title,leftUp,leftDown,rightUp,rightDown;
     const screenWidth = this.state.width;
     if (screenWidth >= 1024){
       title = <img src={"./assets/images/title-hor.png"} />
+      leftUp = <img src={"./assets/images/left-up.png"} className="w-[325px] h-[230px] lg:w-[560px] lg:h-[446px] bg-white-500"/>
+      leftDown = <img src={"./assets/images/left-down.png"} className="w-[325px] h-[230px] lg:w-[560px] lg:h-[578px] bg-white-500"/>
+      rightUp = <img src={"./assets/images/right-up.png"} className="w-[325px] h-[230px] lg:w-[560px] lg:h-[628px] bg-white-500"/>
+      rightDown = <img src={"./assets/images/right-down.png"} className="w-[325px] h-[230px] lg:w-[560px] lg:h-[396px] bg-white-500"/>
     }else{
       title = <img src={"./assets/images/title-ver.png"} />
+      leftUp = <img src={"./assets/images/left-up-small.png"} className="w-[325px] h-[230px] lg:w-[560px] lg:h-[446px] bg-white-500"/>
+      leftDown = <img src={"./assets/images/right-up-small.png"} className="w-[325px] h-[230px] lg:w-[560px] lg:h-[628px] bg-white-500"/>
+      rightUp = <img src={"./assets/images/left-down-small.png"} className="w-[325px] h-[230px] lg:w-[560px] lg:h-[578px] bg-white-500"/>
+      rightDown = <img src={"./assets/images/right-down-small.png"} className="w-[325px] h-[230px] lg:w-[560px] lg:h-[396px] bg-white-500"/>
     }
     
 
@@ -35,22 +43,21 @@ class Main extends React.Component {
       <div className="flex flex-col lg:flex-row items-center">
         <div className="flex-col">
           <div className="w-[325px] h-[230px] lg:w-[560px] lg:h-[446px] bg-white-500">
-            <img src={"./assets/images/left-up.png"} className="w-[325px] h-[230px] lg:w-[560px] lg:h-[446px] bg-white-500"/>
-            {/* Picture 1 */}
+            {leftUp}
           </div>
           {/* <div className="horizontal-gap"/> */}
           <div className="w-[325px] h-[230px] lg:w-[560px] lg:h-[578px] bg-blue-500">
-            {/* <img src={"./assets/images/left-down.png"} className="w-[325px] h-[230px] lg:w-[560px] lg:h-[578px] bg-white-500"/> */}
+            {leftDown}
           </div>
         </div>
         {/* <div className="vertical-gap"/> */}
         <div className="flex-col">
           <div className="w-[325px] h-[230px] lg:w-[560px] lg:h-[628px] bg-yellow-500">
-            {/* <img src={"./assets/images/right-up.png"} className="w-[325px] h-[230px] lg:w-[560px] lg:h-[628px] bg-white-500"/> */}
+            {rightUp}
           </div>
           {/* <div className="horizontal-gap"/> */}
           <div className="w-[325px] h-[230px] lg:w-[560px] lg:h-[396px] bg-green-500">
-            {/* <img src={"./assets/images/right-down.png"} className="w-[325px] h-[230px] lg:w-[560px] lg:h-[396px] bg-white-500"/> */}
+            {rightDown}
           </div>
         </div>
       </div>
