375f183ab48be0365f9d344fcb5bf65e6311af1b
Work <Work@Hengs-MacBook-Air.local>
2023-04-12 12:26:41 -0400

    remove debug color

--- virgocx/src/pages/App.tsx
+++ virgocx/src/pages/App.tsx
@@ -22,16 +22,16 @@ class Main extends React.Component {
     const screenWidth = this.state.width;
     if (screenWidth >= 1024){
       title = <img src={"./assets/images/title-hor.png"} />
-      leftUp = <img src={"./assets/images/left-up.png"} className="w-[560px] h-[446px] bg-white-500"/>
-      leftDown = <img src={"./assets/images/left-down.png"} className="w-[560px] h-[578px] bg-white-500"/>
-      rightUp = <img src={"./assets/images/right-up.png"} className="w-[560px] h-[628px] bg-white-500"/>
-      rightDown = <img src={"./assets/images/right-down.png"} className="w-[560px] h-[396px] bg-white-500"/>
+      leftUp = <img src={"./assets/images/left-up.png"} className="w-[560px] h-[446px]"/>
+      leftDown = <img src={"./assets/images/left-down.png"} className="w-[560px] h-[578px]"/>
+      rightUp = <img src={"./assets/images/right-up.png"} className="w-[560px] h-[628px] "/>
+      rightDown = <img src={"./assets/images/right-down.png"} className="w-[560px] h-[396px] "/>
     }else{
       title = <img src={"./assets/images/title-ver.png"} />
-      leftUp = <img src={"./assets/images/left-up-small.png"} className="w-[325px] h-[230px] bg-white-500"/>
-      leftDown = <img src={"./assets/images/right-up-small.png"} className="w-[325px] h-[230px] bg-white-500"/>
-      rightUp = <img src={"./assets/images/left-down-small.png"} className="w-[325px] h-[230px] bg-white-500"/>
-      rightDown = <img src={"./assets/images/right-down-small.png"} className="w-[325px] h-[230px] bg-white-500"/>
+      leftUp = <img src={"./assets/images/left-up-small.png"} className="w-[325px] h-[230px] "/>
+      leftDown = <img src={"./assets/images/right-up-small.png"} className="w-[325px] h-[230px] "/>
+      rightUp = <img src={"./assets/images/left-down-small.png"} className="w-[325px] h-[230px] "/>
+      rightDown = <img src={"./assets/images/right-down-small.png"} className="w-[325px] h-[230px] "/>
     }
     
 
@@ -42,18 +42,18 @@ class Main extends React.Component {
       </div>
       <div className="flex flex-col lg:flex-row items-center">
         <div className="flex-col">
-          <div className="w-[325px] h-[230px] lg:w-[560px] lg:h-[446px] bg-white-500">
+          <div className="w-[325px] h-[230px] lg:w-[560px] lg:h-[446px] ">
             {leftUp}
           </div>
-          <div className="w-[325px] h-[230px] lg:w-[560px] lg:h-[578px] bg-blue-500">
+          <div className="w-[325px] h-[230px] lg:w-[560px] lg:h-[578px] ">
             {leftDown}
           </div>
         </div>
         <div className="flex-col">
-          <div className="w-[325px] h-[230px] lg:w-[560px] lg:h-[628px] bg-yellow-500">
+          <div className="w-[325px] h-[230px] lg:w-[560px] lg:h-[628px] ">
             {rightUp}
           </div>
-          <div className="w-[325px] h-[230px] lg:w-[560px] lg:h-[396px] bg-green-500">
+          <div className="w-[325px] h-[230px] lg:w-[560px] lg:h-[396px] ">
             {rightDown}
           </div>
         </div>
